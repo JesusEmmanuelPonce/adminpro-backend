0c9d8f1b57787afd4cd28ccb8ec64d424ca915cf
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2021-07-18 09:18:53 -0500

    validation

--- controllers/users.js
+++ controllers/users.js
@@ -14,11 +14,28 @@ exports.createUser = async(req, res) => {
 
     const { name, email, password } = req.body;
 
-    const user = new User(req.body);
-    
-    await user.save();
+    try {
 
-    res.status(200).json({
-        status: 200,
-    });
+        const isEmail = await User.findOne({ email });
+
+        if(isEmail){
+            return res.status(400).json({
+                success: true,
+                msg: "Email already registered",
+            });
+        }
+
+        const user = new User(req.body);
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            msg: "User created",
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+        });
+    }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
+const { check } = require("express-validator");
 const { Router } = require("express");
+
+const { validationField } = require("../middlewares/validation-fields");
 const {
     getUser,
     createUser
@@ -8,6 +11,12 @@ const router = Router();
 
 router.get("/", getUser);
 
-router.post("/create", createUser);
+router.post("/create",
+    [
+        check("name", "name is required").not().isEmpty(),
+        check("password", "password is required").not().isEmpty(),
+        check("email", "email is required").isEmail(),
+        validationField,
+    ], createUser);
 
 module.exports = router;
